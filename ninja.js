cb45887ffae9ec1e3db8a879d36a2c8a7f3f8966
Jorge Leon <84067777+jorge2751@users.noreply.github.com>
2023-03-07 11:20:52 -0800

    Fixes

--- ninja.js
+++ ninja.js
@@ -25,14 +25,17 @@ class Ninja {
 //Extend the Ninja class and create the Sensei class. A Sensei should have 200 Health, 10 speed, and 10 strength by default. In addition, a Sensei should have a new attribute called wisdom, and the default should be 10. Finally, add the speakWisdom() method. speakWisdom() should call the drinkSake() method from the Ninja class, before console.logging a wise message.
 
 class Sensei extends Ninja {
-    constructor(name, health=100, speed=10, strength = 10) {
-        super(name, health, speed, strength)
+    constructor(name, health=100) {
+        super(name, health)
+        this.speed = 10
+        this.strength = 10
         this.wisdom = 10
     }
 
     speakWisdom(ninja) {
         super.drinkSake()
-        console.log(`The key to happiness is not in having everything ${ninja.name} son, but in finding joy in what you have.`)
+        console.log(`The key to happiness is not in having everything, ${ninja.name}-san, but in finding joy in what you have.`)
+        return this
     }
 }
 
@@ -40,4 +43,4 @@ const ninja1 = new Ninja('Jorge', 50)
 const sensei1 = new Sensei('Master Roku')
 
 ninja1.showStats().drinkSake().drinkSake().showStats()
-sensei1.drinkSake().speakWisdom(ninja1)
+sensei1.drinkSake().speakWisdom(ninja1).showStats()
